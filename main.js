e8096e02aa93913f1d11dc44875fc17456ab484b
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-04-27 19:00:31 +0200

    Main modified

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
 
@@ -10,3 +10,7 @@ app.listen(port, () => console.log(`Server started at http://localhost:${port}`)
 app.get("/", (request, response) => {
     response.json("Server works!");
 });
+
+app.get("/login/:user/:psswd"), (request,response) =>{
+	response.json("siiiii");
+}
