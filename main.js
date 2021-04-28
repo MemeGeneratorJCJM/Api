65bbf23c0712ad4282706f2e27a7254d88402640
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-04-28 16:32:48 +0200

    Solved test bug

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
-
+app.listen(PORT, () => {console.log(`Server started at http://localhost:${PORT}`)});
 
 // ======== Endpoints ======== //
 app.get("/", (request, response) => {
     response.json("Server works!");
 });
 
-app.get("/login/:user/:psswd"), (request,response) =>{
+app.get("/login/:user/:psswd", (request,response) => {
 	response.json("siiiii");
-}
+});
