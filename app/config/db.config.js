8d24a05e98128d0b29ced13fe5f3eb017684f5c9
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-10 18:31:19 +0200

    heroku push

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "memeGenerator",
-  PASSWORD: "admin",
-  DB: "test"
+  HOST: "sql311.byethost14.com",
+  USER: "b14_28587920",
+  PASSWORD: "B7z5w2000",
+  DB: "b14_28587920_meme_generator"
 };
+
+//module.exports = {  HOST: "localhost",  USER: "memeGenerator",  PASSWORD: "admin",  DB: "test"};
