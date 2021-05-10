9c1285e0f95e8cf801dcec35a1aa6144e0dae012
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-10 15:52:36 +0200

    Cors Added

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 const app = express();
+var cors = require('cors');
 
 // ======== App ======== //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
+var corsOptions = {
+  origin: function (origin, callback) {
+    // db.loadOrigins is an example call to load
+    // a list of origins from a backing database
+    db.loadOrigins(function (error, origins) {
+      callback(error, origins)
+    })
+  }
+}
+
+// ======== Basic Endpoint ======== //
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido a la API de MemeGenerator" });
 });
@@ -18,3 +31,4 @@ require("./app/routes/customer.routes.js")(app);
 
 // ======== Listener ======== //
 app.listen(PORT, () => {console.log(`Server started at http://localhost:${PORT}`)});
+
