5ee51be96220be9a700c8d01b94df1bb8bb55c3e
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-12 17:30:35 +0200

    API changes

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -112,7 +112,8 @@ Customer.removeAll = result => {
 
 Customer.login = (email, password, result) => {
   sql.query(
-    "SELECT * FROM users WHERE email like '"+email+"' and password like '"+password+"'",(err,res) => {
+    "SELECT username FROM users WHERE idUser = 1;",(err,res) => {
+    //"SELECT username FROM users WHERE email like '"+email+"' and password like '"+password+"'",(err,res) => {
       if (err) {
       console.log("error: ", err);
       console.log("email: "+email+ " password: "+password)
