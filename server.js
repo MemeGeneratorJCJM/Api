22030b52881b62ed30e722d3c99e50c961ed331f
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-14 18:20:38 +0200

    solving retrieve image

--- server.js
+++ server.js
@@ -63,6 +63,51 @@ app.post("/image-upload", (request, response) => {
     });
 
 });
+// persist image
+app.post("/persist-image", (request, response) => {
+  // collected image from a user
+  const data = {
+    title: request.body.title,
+    image: request.body.image
+  }
+
+  // upload image here
+  cloudinary.uploader.upload(data.image)
+  .then((image) => {
+    db.pool.connect((err, client) => {
+      // inset query to run if the upload to cloudinary is successful
+      const insertQuery = 'INSERT INTO images (title, cloudinary_id, image_url) VALUES($1,$2,$3) RETURNING *';
+      const values = [data.title, image.public_id, image.secure_url];
+
+      // execute query
+      client.query(insertQuery, values)
+      .then((result) => {
+        result = result.rows[0];
+
+        // send success response
+        response.status(201).send({
+          status: "success",
+          data: {
+            message: "Image Uploaded Successfully",
+            title: result.title,
+            cloudinary_id: result.cloudinary_id,
+            image_url: result.image_url,
+          },
+        })
+      }).catch((e) => {
+        response.status(500).send({
+          message: "failure",
+          e,
+        });
+      })
+    })  
+  }).catch((error) => {
+    response.status(500).send({
+      message: "failure",
+      error,
+    });
+  });
+});
 
 app.get("/retrieve-image/:cloudinary_id", (request, response) => {
   // data from user
@@ -70,7 +115,7 @@ app.get("/retrieve-image/:cloudinary_id", (request, response) => {
 
   db.pool.connect((err, client) => {
     // query to find image
-    const query = "SELECT * FROM images WHERE cloudinary_id = "+ cloudinary_id;
+    const query = "SELECT * FROM images WHERE cloudinary_id = $1";
     const value = [cloudinary_id];
 
     // execute query
@@ -97,5 +142,6 @@ app.get("/retrieve-image/:cloudinary_id", (request, response) => {
       });
   });
 });
+
 
 module.exports = app;
