de10c0e04a1104917a3377d2fbc855ce7c055f25
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-14 16:06:56 +0200

    try to solve result on create user

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -156,9 +156,9 @@ Customer.login = (email, password, result) => {
     // hay que tratar cuando el user no existe, devuelve status 200 OK
     if (!result || !result.rows || !result.rows.length){
       console.log("error: ", err);
-      result(null,res);
+      result(null,"si"+res);
     }else{
-      result(null, res);
+      result(null, "no"+res);
     }
   });
 };
