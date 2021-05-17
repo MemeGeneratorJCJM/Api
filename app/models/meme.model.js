49efaa8b22976f9894fae98d2e7e4ea09d2744c7
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-17 18:33:18 +0200

    solving problems

--- app/models/meme.model.js
+++ app/models/meme.model.js
@@ -21,7 +21,22 @@ Meme.create = (name,route,idCategory,result) => {
 };
 
 Meme.findMemeByName = (name, result) => {
+  sql.query("SELECT * FROM memes WHERE name like $1;",[name], (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
 
+    if (res.length) {
+      console.log("found meme: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Meme with the id
+    result({ kind: "not_found" }, null);
+  });
 };
 
 Meme.deleteMemeById = (idMeme, result) => {
