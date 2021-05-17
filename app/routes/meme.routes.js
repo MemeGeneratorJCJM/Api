12d8d11f060f6dbd2656cfeceeae33de39b67eae
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-17 17:46:19 +0200

    image download solve

--- app/routes/meme.routes.js
+++ app/routes/meme.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const meme = require("../controllers/meme.controller.js");
 
   //Create a new Meme
-  //app.post("/meme/create", meme.createMeme);
+  app.post("/meme/create", meme.createMeme);
 
   //Find a Meme by Name
   app.get("/meme/findByName/:name", meme.findByName);
